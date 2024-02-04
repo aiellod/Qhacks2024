e6fd62b2a23ed882e3a957bed1ea8c64a63247a3
Rymazon <Ryanscomazzon@gmail.com>
2024-02-04 03:03:45 -0500

    TOOLTIPS SHOW

--- modules/keyintercept.py
+++ modules/keyintercept.py
@@ -4,13 +4,14 @@ import queue
 import settings
 
 def key_press(event):
-    # If a key is pressed and the message queue is not empty
-    a = event.event_type == keyboard.KEY_DOWN and len(event.name) == 1 and settings.KeyQueue.qsize() > 0
-    b = settings.enabled == True and not event.name in ['8', '9', '0'] 
+    # If a key is pressed and the message queue is not empty )
+    a = event.event_type == keyboard.KEY_DOWN and len(event.name) == 1 
+    b = settings.enabled == True and settings.KeyQueue.qsize() > 0
     if a and b:
         sleep(0.000001)
         keyboard.press_and_release('backspace')
-        keyboard.write(settings.KeyQueue.get())
+        if not event.name in ['*', '(', ')']:
+            keyboard.write(settings.KeyQueue.get())
 
 def init():
     keyboard.hook(key_press)
